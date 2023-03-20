96ec724403ce8bdf808bf7563a5e777eb536758d
MoseNjau <moses.njau@students.jkuat.ac.ke>
2023-03-20 09:47:31 -0700

    yes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
 #!/bin/bash
 #include <stdio.h>
 #include <unistd.h>
-/* main contain the entry points
- * return (1) implies success */
+/**
+*  main contain the entry points
+*
+* return (1) implies success 
+*/
 
 int main(void)
 {
-  write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 59);
-  return (1);
+	write(2, 
+	"and that piece of art is useful" - Dora Korpar, 2015-10-
+19\n", 59);
+	return (1);
 }

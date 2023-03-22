00cf0eb7b6c09cde203e9c0ea4c3761d501f82b2
MoseNjau <moses.njau@students.jkuat.ac.ke>
2023-03-22 03:38:59 -0700

    perfect done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include<stdio.h>
-
+#include "main.h"
 /**
- * main - Entry point.
- * Description: prints "_putchar"
- * Return: Always (0) success
+ * main - Entry point
+ *
+ * Description: prints _putchar
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }

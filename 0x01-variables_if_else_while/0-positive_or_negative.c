a1d0ada32324dac8d966d08672e0438e895763d8
MoseNjau <moses.njau@students.jkuat.ac.ke>
2023-03-23 13:39:21 -0700

    addition

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,24 @@
+#include<stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
-
 /**
-* main we will identfy if the number is +ve or -ve
-*
-* Return 0 is always success
-*/
+ * main - Entry point
+ *
+ * Description: Assigns random number
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
-	{
-		printf("%d is a +ve num\n", n);
-	}
-	else if(n == 0)
-	{
-		printf("%d is zero\n",n);
-	}
+	if (n < 0)
+		printf("%d is negative\n", n);
+	else if (n > 0)
+		printf("%d is positive\n", n);
 	else
-	{
-		printf("%d is a -ve number\n";
-	}
+		printf("%d is zero\n", n);
 	return (0);
-
 }

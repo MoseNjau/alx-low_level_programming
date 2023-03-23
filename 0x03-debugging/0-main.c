17daa16047e1a4f5698bc94688865880a9a2a312
MoseNjau <moses.njau@students.jkuat.ac.ke>
2023-03-23 15:50:23 -0700

    debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	int j;
+
+	j = 0;
+
+	positive_or_negative(j);
+	return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int k;
+
+	printf("Infinite loop incoming :(\n");
+
+	k = 0;
+
+	/**
+	 * while (k < 10)
+	 * {
+	 * putchar(k);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}

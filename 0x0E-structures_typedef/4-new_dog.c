9127114203d2de9b46c310a08685a8b6daa40d02
MoseNjau <moses.njau@students.jkuat.ac.ke>
2023-10-09 14:34:44 -0700

    second commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,45 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
-#include "stdlib.h"
+
 /**
- * new_dog - creates a new dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * new_dog - Creates a new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  *
- * Return: pointer to new dog
+ * Return: Pointer to the newly created dog (dog_t)
+ *         NULL if memory allocation fails or if name/owner is NULL
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-unsigned int p, q, i;
-dog_t *dog;
-if (name == NULL || owner == NULL)
-return (NULL);
-dog = malloc(sizeof(dog_t));
-if (dog == NULL)
-return (NULL);
-for (p = 0; name[p]; p++)
-;
-p++;
-dog->name = malloc(p *sizeof(char));
-if (dog->name == NULL)
-{
-free(dog);
-return (NULL);
-}
-for (i = 0; i < nl; i++)
-dog->name[i] = name[i];
-dog->age = age;
-for (q = 0; owner[q]; q++)
-;
-q++;
-dog->owner = malloc(q *sizeof(char));
-if (dog->owner == NULL)
-{
-free(dog->name);
-free(dog);
-return (NULL);
-}
-for (i = 0; i < q; i++)
-dog->owner[i] = owner[i];
-return (dog);
+	dog_t *new_dog_ptr;
+	char *new_name, *new_owner;
+	int name_len = 0, owner_len = 0, i;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	while (name[name_len])
+		name_len++;
+	while (owner[owner_len])
+		owner_len++;
+
+	new_dog_ptr = malloc(sizeof(dog_t));
+	new_name = malloc(name_len + 1);
+	new_owner = malloc(owner_len + 1);
+
+	if (new_dog_ptr == NULL || new_name == NULL || new_owner == NULL)
+	{
+		free(new_dog_ptr);
+		free(new_name);
+		free(new_owner);
+		return (NULL);
+	}
+
+	for (i = 0; i <= name_len; i++)
+		new_name[i] = name[i];
+	for (i = 0; i <= owner_len; i++)
+		new_owner[i] = owner[i];
+
+	new_dog_ptr->name = new_name;
+	new_dog_ptr->age = age;
+	new_dog_ptr->owner = new_owner;
+
+	return (new_dog_ptr);
 }

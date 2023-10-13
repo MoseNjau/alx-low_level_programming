936d2c0d82505ba47c4316d3efa68bdaa498b5e5
MoseNjau <moses.njau@students.jkuat.ac.ke>
2023-10-13 12:30:02 -0700

    variadic functions commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,17 @@
-int _putchar(char c);
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+#include <stdio.h>
+#include <stdarg.h>
+
+typedef struct storage
+{
+	char *chr;
+	void (*funct)(va_list);
+}str;
 
 int sum_them_all(const unsigned int n, ...);
-
 void print_numbers(const char *separator, const unsigned int n, ...);
-
 void print_strings(const char *separator, const unsigned int n, ...);
-
 void print_all(const char * const format, ...);
+
+#endif

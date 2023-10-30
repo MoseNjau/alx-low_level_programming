cb7c8e524486add82e41fa66b7a030bb8226d608
MoseNjau <moses.njau@students.jkuat.ac.ke>
2023-10-30 22:22:59 +0300

    file descriptor

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 #include <unistd.h>
+#include <stddef.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);

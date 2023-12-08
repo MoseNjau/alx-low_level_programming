c615edef92f926cb598ef2ed87755fadddb269b8
MoseNjau <moses.njau@students.jkuat.ac.ke>
2023-12-08 23:29:12 +0300

    double linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * get_dnodeint_at_index - get node at index
+ * @head: head
+ * @index: index
+ * Return: address of node or NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int j;
+
+	if (head == NULL)
+		return (NULL);
+	current = head;
+	j = 0;
+	while (current)
+	{
+		if (j == index)
+			return (current);
+		current = current->next;
+		j++;
+	}
+	return (NULL);
+}
